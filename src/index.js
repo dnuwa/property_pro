a45be1578e46a94bbdcd66751f901e1f09f222ac
dnuwa <daniel.nuwa@gmail.com>
2020-07-07 15:13:43 +0300

    add react botstrap to simplify component creation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import store from "./store";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
